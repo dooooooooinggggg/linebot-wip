d7b0554edae2949a8d7b1780cd0db28716f4e49f
ishikawatatsunori <dooooooooinggggg@gmail.com>
2016-06-30 21:20:49 +0900

    とりあえず、準備だけ

--- linebot.py
+++ linebot.py
@@ -5,8 +5,7 @@ import sys
  
 if __name__=='__main__':
  
-    #print 'parameter1 is ' + sys.argv[1]
-    #print 'parameter2 is ' + sys.argv[2]
-    #print 'result is OK!'
-    #print 'result is NG!'
-    print 'ok'#テスト用
+    print 'parameter1 is ' + sys.argv[1]
+    print 'parameter2 is ' + sys.argv[2]
+    print 'result is OK!'
+    print 'result is NG!'
--- linebot_to_python.php
+++ linebot_to_python.php
@@ -1,13 +1,31 @@
 <?php
 // file name: call_python.php\
 // 引用:http://freetech-e.com/html/callpython.htm
-    $fullPath = 'python linebot.py abcd 1234';
-    exec($fullPath, $outpara);
-    echo '<PRE>';
-    var_dump($fullPath);
-    var_dump($outpara[0]);
-    var_dump($outpara[1]);
-    var_dump($outpara[2]);
-    var_dump($outpara[3]);
-    echo '<PRE>';
+$test='xxx';
+$test2="yyy";
+$fullPath = "python linebot.py $test $test2";
+exec($fullPath, $outpara);
+//$outparaには、printされたものがくる
+echo '<PRE>';
+var_dump($fullPath);
+var_dump($outpara[0]);
+var_dump($outpara[1]);
+var_dump($outpara[2]);
+var_dump($outpara[3]);
+echo '<PRE>';
+
+
+$json_string=file_get_contents('php://input');
+$json_object=json_decode($json_string);
+$content=$json_object->result{0}->content;//これが、基本的な内容
+$text=$content->text;//送られてきた文章
+$to=$content->from;//誰から来たか（変な文字列）
+$message_id=$content->id;
+$content_type=$content->contentType;//コンテントタイプ
+
+exec($fullPath,$outpara);
+
+
+
+
 ?>
