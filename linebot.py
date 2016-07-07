df0f534ecec59c2b458e726214928cde9cc94288
ishikawatatsunori <dooooooooinggggg@gmail.com>
2016-07-07 17:38:22 +0900

    なぜか動かない
    php->pythonはうまくいくが
    python->phpがうまくいかない

--- linebot.py
+++ linebot.py
@@ -1,19 +1,33 @@
 # file name: linebot.py
 # coding:utf-8
  
-import sys
+import sys,urllib2
+import json
  
 if __name__=='__main__':
- 
 
-    receivetext = sys.argv[1]
+
+	receivetext = sys.argv[1]
+	#receivetext = '460010'
 
     #ここに処理を書く
 
-    
+	if receivetext == 'help':
+		sendtext = "ここを参考に天気コードを入力してください。UPL->http://weather.livedoor.com/forecast/rss/primary_area.xml"
 
-    #ここまで
+	else:
+		#citycode = sys.argv[1]
+		citycode = receivetext
+		resp = urllib2.urlopen("http://weather.livedoor.com/forecast/webservice/json/v1?city=%s" % citycode).read()
+		resp = json.loads(resp)
+		#forecasttext = ''
 
-    sendtext = receivetext#とりあえず
+		#for forecast in resp['forecasts']:
+		#	forecasttext = forecasttext + "\n*************\n"
+		#	forecasttext = forecasttext + forecast['dateLabel']+ "(" +forecast['date'] + ")\n"
+		#	forecasttext = forecasttext + forecast['telop']
 
-    print sendtext#ここでphpに戻している。
+		#sendtext = "*************\n" + resp['title'] + "\n*************\n" + resp['description']['text'] + forecasttext + "\n*************\n"
+		sendtext = resp['description']['text']
+	print sendtext
+	#ここでphpに戻している
